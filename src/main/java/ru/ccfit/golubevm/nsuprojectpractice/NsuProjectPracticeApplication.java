7c77c0e3169885d0b0258c3e21b00b6bc80ecbe2
Xisepe <maxim.golubeff2014@gmail.com>
2024-02-19 20:10:19 +0700

    init

---
+++ src/main/java/ru/ccfit/golubevm/nsuprojectpractice/NsuProjectPracticeApplication.java
@@ -0,0 +1,13 @@
+package ru.ccfit.golubevm.nsuprojectpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsuProjectPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NsuProjectPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ccfit/golubevm/nsuprojectpractice/NsuProjectPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ccfit.golubevm.nsuprojectpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsuProjectPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
